7b9c87bdccf9155c30c76907887d1f2b575fb5ec
Yurii Planhev <planghevyurii@gmail.com>
2025-06-02 21:46:59 +0300

    deploy

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,39 +1,39 @@
-import './styles/App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import "./styles/App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Header from "../widgets/Header/Header";
-import MovieDetails from '../pages/MovieDetails';
-import Home from '../pages/Home';
-import FilterByCategory from '../pages/FilterByCategory';
-import ErrorBoundary from '../pages/ErrorPage';
-import Footer from '../widgets/Footer/Footer';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import MovieDetails from "../pages/MovieDetails";
+import Home from "../pages/Home";
+import FilterByCategory from "../pages/FilterByCategory";
+import ErrorBoundary from "../pages/ErrorPage";
+import Footer from "../widgets/Footer/Footer";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
-
-function ScropllToTop () {
-  const location = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  
-  }, [ location.pathname]);
-}
-
+  function ScropllToTop() {
+    const location = useLocation();
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location.pathname]);
+  }
 
   return (
     <div className="max-w-[1200px] m-auto my-7">
       <ErrorBoundary>
-        <Router>
-          <ScropllToTop/>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/media/:mediaType/:movieId" element={<MovieDetails />} /> 
-            <Route path="/categories" element={<FilterByCategory />} />
-            <Route path="*" element={<div>404 - Страница не найдена</div>} />
-          </Routes>
-          <Footer/>
-        </Router>
+        <ScropllToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="" element={<Navigate to="/" />} />
+          <Route path="/media/:mediaType/:movieId" element={<MovieDetails />} />
+          <Route path="/categories" element={<FilterByCategory />} />
+        </Routes>
+        <Footer />
       </ErrorBoundary>
     </div>
   );
